c2891e8f32ffb7d7e3315a9c8b353030233bc6e6
Konrad-Gumos <konradgumos@student.agh.edu.pl>
2025-04-03 17:56:41 +0200

    v2

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,59 +1,63 @@
-import random
 import matplotlib.pyplot as plt
-import math
-
 import numpy as np
 
 
 def drawNumber(n):
-    x = []
-    y = []
-    for i in range(n):
-        x.append(random.random())
-        y.append(random.random())
+    # drawing two sets of random numbers - coordinates
+    x = np.random.rand(n)
+    y = np.random.rand(n)
     return x,y
 
-def inCircVerify(x,y,ax1,ax2):
-    inCircX = []
-    inCircY = []
-    outCircX = []
-    outCircY = []
-    #circle = plt.Circle((0,0), 1, color='black', fill=False)
+def inCircVerify(x,y):
+    global inCircle # indicator whether the point is placed within circle or outside
+    inCircle = y <= np.sqrt(1 - x ** 2)
+    calc_pi = (inCircle.sum() / inCircle.size) * 4 # calculated value of pi
+    return calc_pi
 
-    for i in range(len(y)):
-        if y[i] <= math.sqrt(1 - x[i]**2):
-            inCircX.append(x[i])
-            inCircY.append(y[i])
-        else:
-            outCircX.append(x[i])
-            outCircY.append(y[i])
+def drawFigures(ax1,ax2,x,y,calc_pi):
+    # function for drawing graphs presenting points, red markers represent points outside circle, while blue are those placed inside
+    circle = plt.Circle((0,0), 1, color='black', fill=False)
+    ax[ax1, ax2].scatter(x[~inCircle], y[~inCircle], color='red', s=5)
+    ax[ax1, ax2].scatter(x[inCircle], y[inCircle], color='blue', s=5)
 
-    calc_pi = len(inCircX)/len(y)*4
-    pis.append(calc_pi)
+    ax[ax1, ax2].add_patch(circle)
 
-    # ax[ax1, ax2].scatter(inCircX, inCircY, color='red',s=5)
-    # ax[ax1, ax2].scatter(outCircX,outCircY,color='blue',s=5)
-    # ax[ax1, ax2].add_patch(circle)
-    # ax[ax1, ax2].set_xlim((0, 1))
-    # ax[ax1, ax2].set_ylim((0, 1))
+    ax[ax1, ax2].set_xlim((0, 1))
+    ax[ax1, ax2].set_ylim((0, 1))
+    ax[ax1, ax2].set_aspect('equal')
+    ax[ax1, ax2].set_title(f'n = {x.size}, pi = {calc_pi}')
+    plt.subplots_adjust(hspace = 0.5)
 
-# [X1,Y1] = drawNumber(100)
-# [X2,Y2] = drawNumber(1000)
-# [X3,Y3] = drawNumber(10000)
-# [X4,Y4] = drawNumber(100000)
-#
-#
-# fig,ax = plt.subplots(2,2)
-# inCircVerify(X1,Y1,0,0)
-# inCircVerify(X2,Y2,0,1)
-# inCircVerify(X3,Y3,1,0)
-# inCircVerify(X4,Y4,1,1)
-# plt.show()
+[X1,Y1] = drawNumber(100)
+[X2,Y2] = drawNumber(1000)
+[X3,Y3] = drawNumber(10000)
+[X4,Y4] = drawNumber(100000)
+
+fig,ax = plt.subplots(2,2)
+drawFigures(0,0,X1,Y1,inCircVerify(X1,Y1))
+drawFigures(0,1,X2,Y2,inCircVerify(X2,Y2))
+drawFigures(1,0,X3,Y3,inCircVerify(X3,Y3))
+drawFigures(1,1,X4,Y4,inCircVerify(X4,Y4))
+plt.show()
+
 pis = []
-for i in range(100,100000):
-    print(i)
-    [X,Y] = drawNumber(i)
-    inCircVerify(X,Y,0,0)
+for k in range(100,1000):
+    [X,Y] = drawNumber(k)
+    pis.append(inCircVerify(X,Y))
 
 plt.plot(pis)
+plt.title("Wykres wyznaczanej wartości dla liczby losowanych punktów")
+plt.hlines(3.14,0,1000,color="red",label="3.14")
+plt.xlabel("Liczba losowanych punktów")
+plt.ylabel("Wyznaczana wartość")
+plt.show()
+
+ns = [100000,250000,500000,750000,1000000]
+data = [[inCircVerify(drawNumber(n)[0],drawNumber(n)[1]) for _ in range(10)] for n in ns]
+print(data)
+plt.boxplot(data)
+plt.hlines(3.14,0,6,color="red",label="3.14",linestyles="dashed")
+# plt.xticks(ns)
+plt.xlabel("Liczba losowanych punktów")
+plt.ylabel("Wyznaczana wartość")
 plt.show()
